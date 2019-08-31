8af4857532a2c2ecef64991d858d73afec4525c9
morolty85 <morolty85@gmail.com>
2019-08-31 21:28:54 +0900

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,14 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript',
-  ],
+  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parserOptions: {
-    parser: '@typescript-eslint/parser',
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+    parser: "@typescript-eslint/parser"
+  }
 };
