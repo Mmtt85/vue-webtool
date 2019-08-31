126f812ef80649f769b66f7e4e82f89c05f7a349
morolty85 <morolty85@gmail.com>
2019-08-31 22:17:20 +0900

    example code

---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login">
+    <b-button>Login</b-button>
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data() {
+    return {
+      form: {
+        id: '',
+        password: ''
+      }
+    };
+  },
+  methods: {
+    onSubmit: e => {
+      e.preventDefault();
+      alert(this.form);
+    },
+    onReset: e => {
+      e.preventDefault();
+      this.form.id = '';
+      this.form.password = '';
+    }
+  }
+})
+export default class Login extends Vue {}
+</script>
+
+<style scoped></style>
